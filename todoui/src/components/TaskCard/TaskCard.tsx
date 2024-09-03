16a6752dfdf9ba79a2dce84f60fb49aa60def64d
marorcas <ortzmc@gmail.com>
2024-09-03 23:56:27 +1000

    Change task cards into editable forms

--- todoui/src/components/TaskCard/TaskCard.tsx
+++ todoui/src/components/TaskCard/TaskCard.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-import { TaskResponse } from "../../services/task-services";
+import { editTaskById, TaskResponse } from "../../services/task-services";
 import styles from "./TaskCard.module.scss";
+import TaskForm from "../TaskForm/TaskForm";
+import { TaskFormData } from "../TaskForm/schema";
 
 interface TaskProps {
     task: TaskResponse;
@@ -8,22 +10,47 @@ interface TaskProps {
 }
 
 const TaskCard = ({ task, onDelete }: TaskProps) => {
-    return(
-        <article 
-            className={styles.TaskCard}
-            key={task.id}
-        >
-            <div className={styles.TaskInfo}>
-                <h2 className={styles.Task}>{task.description}</h2>
-                <h3 className={styles.Category}>category: {task.category?.name ?? 'none'}</h3>
-            </div>
 
-            <div className={styles.Buttons}>
-                <button onClick={() => onDelete(task.id)}>Delete</button>
-                <button>Duplicate</button>
-                <Link to={`tasks/${task.id}/edit`}>Edit</Link>
-            </div>
+    const onSubmit = async (data: TaskFormData) => {
+        console.log(data)
+        const dataTask = editTaskById(task.id, data)
+            .then((task) => {
+                console.log(task)
+            })
+            .catch(() => alert('Failed to update post'));
+        console.log(dataTask)
+    }
+
+    const taskData = {
+        description: task.description,
+        categoryId: task.category ? task.category.id : undefined
+    }
+
+    return(
+        // <article 
+        //     className={styles.TaskCard}
+        //     key={task.id}
+        // >
+        //     <div className={styles.TaskInfo}>
+        //         <h2 className={styles.Task}>{task.description}</h2>
+        //         <h3 className={styles.Category}>category: {task.category?.name ?? 'none'}</h3>
+        //     </div>
+
+        //     <div className={styles.Buttons}>
+        //         <button onClick={() => onDelete(task.id)}>Delete</button>
+        //         <button>Duplicate</button>
+        //         <Link to={`tasks/${task.id}/edit`}>Edit</Link>
+        //     </div>
+        // </article>
+
+        <article>
+            <TaskForm formType="EDIT" defaultValues={taskData} onSubmit={onSubmit} />
+
+            <button onClick={() => onDelete(task.id)}>Delete</button>
+
+            <button>Duplicate</button>
         </article>
+        
     )
 }
 
